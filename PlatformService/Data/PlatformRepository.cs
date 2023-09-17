01e326bfb13b240f362d6d96f5651e513ac4095c
Dariusz Gorski <dariusz.gorski@fortedigital.no>
2023-09-17 11:36:19 +0200

    Added DbContext + model + repository pattern for platforms

---
+++ PlatformService/Data/PlatformRepository.cs
@@ -0,0 +1,33 @@
+using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public class PlatformRepository : IPlatformRepository
+{
+    private readonly AppDbContext _appDbContext;
+
+    public PlatformRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+    
+    public bool SaveChanges()
+    {
+        return _appDbContext.SaveChanges(true) >= 0;
+    }
+
+    public IEnumerable<Platform> GetAllPlatforms()
+    {
+        return _appDbContext.Platforms.ToList();
+    }
+
+    public Platform GetPlatformById(int id)
+    {
+        return _appDbContext.Platforms.FirstOrDefault(platform => platform.Id == id) ?? new Platform();
+    }
+
+    public void CreatePlatform(Platform platform)
+    {
+        _appDbContext.Platforms.Add(platform);
+    }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseInMemoryDatabase("InMemoryDb"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
